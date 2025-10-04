9bfbee43cc5aea37416ef5bf15d3f0fc5676b79c
Ceres <47678781+Ilysen@users.noreply.github.com>
2025-10-04 03:14:21 -0400

    clean up this travesty a bit hopefully

--- src/ilysen/fullspectrumsweep/campaign/abilities/FullSpectrumSweepAbility.java
+++ src/ilysen/fullspectrumsweep/campaign/abilities/FullSpectrumSweepAbility.java
@@ -2,7 +2,6 @@ package ilysen.fullspectrumsweep.campaign.abilities;
 
 import java.awt.Color;
 import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,20 +11,25 @@ import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.LocationAPI;
 import com.fs.starfarer.api.campaign.SectorEntityToken;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
+import com.fs.starfarer.api.campaign.SectorEntityToken.VisibilityLevel;
 import com.fs.starfarer.api.impl.campaign.abilities.BaseDurationAbility;
+import com.fs.starfarer.api.impl.campaign.ids.Pings;
 import com.fs.starfarer.api.impl.campaign.ids.Tags;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
 public class FullSpectrumSweepAbility extends BaseDurationAbility {
+	public static final String FRONT_END_TEXT = "Full-spectrum sweep";
+	public static final String FLAG_NAME = "$ilysen_fullSpectrumSweep_didFssInSystem";
 	public static final float DETECTABILITY_RANGE_BONUS = 5000f;
-	public static final float COMBAT_READINESS_HIT_MULT = 0.05f;
-	protected boolean performed = false;
+
+	protected boolean isPerformingScan = false;
 	protected boolean hasScannedCurSystem = false;
 	protected boolean systemComplete = false;
 
-	public FullSpectrumSweepAbility() {
-	}
+	private float _padding = 10f;
+
+	public FullSpectrumSweepAbility() { }
 
 	@Override
 	public String getSpriteName() {
@@ -46,29 +50,23 @@ public class FullSpectrumSweepAbility extends BaseDurationAbility {
 
 	@Override
 	protected void activateImpl() {
-		if (this.entity.isInCurrentLocation()) {
-			/*
-			 * SectorEntityToken.VisibilityLevel level =
-			 * entity.getVisibilityLevelToPlayerFleet();
-			 * if (level != VisibilityLevel.NONE) {
-			 * Global.getSector().addPing(entity, "fss");
-			 * }
-			 */
-			this.performed = false;
+		if (entity.isInCurrentLocation()) {
+			if (entity.getVisibilityLevelToPlayerFleet() != VisibilityLevel.NONE)
+				Global.getSector().addPing(entity, Pings.REMOTE_SURVEY);
+			isPerformingScan = true;
 		}
 	}
 
 	@Override
 	protected void applyEffect(float arg0, float arg1) {
-		CampaignFleetAPI fleet = this.getFleet();
+		CampaignFleetAPI fleet = getFleet();
 		if (fleet == null)
 			return;
-		fleet.getStats().getFleetwideMaxBurnMod().modifyMult(this.getModId(), 0.0F, "Full-spectrum sweep");
-		fleet.getStats().getDetectedRangeMod().modifyFlat(this.getModId(), DETECTABILITY_RANGE_BONUS * level,
-				"Full-spectrum sweep");
-		fleet.getStats().getAccelerationMult().modifyMult(this.getModId(), 1.0f + 3.0f * level);
-		if (!this.performed && level >= 1.0F) {
-			fleet.getStarSystem().getMemoryWithoutUpdate().set("$fss_didFssInSystem", true);
+		fleet.getStats().getFleetwideMaxBurnMod().modifyMult(getModId(), 0f, FRONT_END_TEXT);
+		fleet.getStats().getDetectedRangeMod().modifyFlat(getModId(), DETECTABILITY_RANGE_BONUS * level, FRONT_END_TEXT);
+		fleet.getStats().getAccelerationMult().modifyMult(getModId(), 1f + (3f * level));
+		if (isPerformingScan && level >= 1f) {
+			fleet.getStarSystem().getMemoryWithoutUpdate().set(FLAG_NAME, true);
 			LocationAPI loc = fleet.getContainingLocation();
 			Map<String, List<SectorEntityToken>> scannedEntities = GetAllUndiscoveredEntities(loc);
 			int totalSize = 0;
@@ -85,22 +83,22 @@ public class FullSpectrumSweepAbility extends BaseDurationAbility {
 						Misc.getHighlightColor(),
 						Misc.getTextColor());
 			}
-			this.performed = true;
+			isPerformingScan = false;
 		}
 	}
 
 	@Override
 	protected String getActivationText() {
-		return "Full-spectrum sweep";
+		return FRONT_END_TEXT;
 	}
 
 	@Override
 	protected void cleanupImpl() {
-		CampaignFleetAPI fleet = this.getFleet();
+		CampaignFleetAPI fleet = getFleet();
 		if (fleet != null) {
-			fleet.getStats().getDetectedRangeMod().unmodify(this.getModId());
-			fleet.getStats().getFleetwideMaxBurnMod().unmodify(this.getModId());
-			fleet.getStats().getAccelerationMult().unmodify(this.getModId());
+			fleet.getStats().getDetectedRangeMod().unmodify(getModId());
+			fleet.getStats().getFleetwideMaxBurnMod().unmodify(getModId());
+			fleet.getStats().getAccelerationMult().unmodify(getModId());
 		}
 	}
 
@@ -113,20 +111,19 @@ public class FullSpectrumSweepAbility extends BaseDurationAbility {
 	public boolean isUsable() {
 		if (!super.isUsable())
 			return false;
-		CampaignFleetAPI fleet = this.getFleet();
+		CampaignFleetAPI fleet = getFleet();
 		if (fleet == null || fleet.isInHyperspace() || fleet.isInHyperspaceTransition())
 			return false;
 		StarSystemAPI system = fleet.getStarSystem();
-		return system != null && !system.getMemoryWithoutUpdate().contains("$fss_didFssInSystem");
+		return system != null && !system.getMemoryWithoutUpdate().contains(FLAG_NAME);
 	}
 
 	@Override
 	public void createTooltip(TooltipMakerAPI tooltip, boolean expanded) {
-		CampaignFleetAPI fleet = this.getFleet();
+		CampaignFleetAPI fleet = getFleet();
 		if (fleet == null)
 			return;
 		StarSystemAPI system = fleet.getStarSystem();
-		float padding = 10.0F;
 		Color highlight = Misc.getHighlightColor();
 		Color negative = Misc.getNegativeHighlightColor();
 		Color gray = Misc.getGrayColor();
@@ -138,38 +135,38 @@ public class FullSpectrumSweepAbility extends BaseDurationAbility {
 				if (Misc.random.nextInt(100) == 1) {
 					switch (Misc.random.nextInt(5)) {
 						case 1:
-							tooltip.addPara("%s", padding, negative,
+							tooltip.addPara("%s", _padding, negative,
 									"WARN: drive bubble pressure exceeds safe tolerance by (10^2 * 3.6821); recommend sensor shutdown");
 							break;
 						case 2:
-							tooltip.addPara("", padding, negative, "WARN: sensor error overflow");
+							tooltip.addPara("", _padding, negative, "WARN: sensor error overflow");
 							break;
 						case 3:
 							for (int i = 0; i < Misc.random.nextInt(5); i++) {
-								tooltip.addPara("%s", padding, negative, "WARN: invalid input");
+								tooltip.addPara("%s", _padding, negative, "WARN: invalid input");
 							}
 							break;
 						case 4:
-							tooltip.addPara("%s", padding, negative, "WARN: ambient temperature exceeds 200C");
+							tooltip.addPara("%s", _padding, negative, "WARN: ambient temperature exceeds 200C");
 							break;
 						default:
-							tooltip.addPara("%s", padding, negative, "WARN: connection lost");
+							tooltip.addPara("%s", _padding, negative, "WARN: connection lost");
 							break;
 					}
 				} else {
 					for (int i = 0; i < 20; i++) {
 						spooky += (char) (Misc.random.nextInt('@' - '!'));
 					}
-					tooltip.addPara(spooky, padding);
+					tooltip.addPara(spooky, _padding);
 				}
 				return;
 			}
 		} else {
-			tooltip.addSpacer(padding);
+			tooltip.addSpacer(_padding);
 		}
 
 		if (!Global.CODEX_TOOLTIP_MODE && system != null
-				&& system.getMemoryWithoutUpdate().contains("$fss_didFssInSystem")) {
+				&& system.getMemoryWithoutUpdate().contains(FLAG_NAME)) {
 			hasScannedCurSystem = true;
 			LocationAPI loc = fleet.getContainingLocation();
 			Map<String, List<SectorEntityToken>> scannedEntities = GetAllUndiscoveredEntities(loc);
@@ -178,19 +175,19 @@ public class FullSpectrumSweepAbility extends BaseDurationAbility {
 				totalSize += subset.size();
 			}
 			if (totalSize == 0) {
-				tooltip.addPara("%s", padding, Misc.getPositiveHighlightColor(),
+				tooltip.addPara("%s", _padding, Misc.getPositiveHighlightColor(),
 						"All objects in this system have been discovered.");
 				return;
 			} else {
 				tooltip.addPara("Undiscovered objects in this system:",
-						padding);
+						_padding);
 				List<SectorEntityToken> debris = scannedEntities.get(Tags.DEBRIS_FIELD);
 				List<SectorEntityToken> lowSources = scannedEntities.get(Tags.NEUTRINO_LOW);
 				List<SectorEntityToken> medSources = scannedEntities.get(Tags.NEUTRINO);
 				List<SectorEntityToken> highSources = scannedEntities.get(Tags.NEUTRINO_HIGH);
 				List<SectorEntityToken> stations = scannedEntities.get(Tags.STATION);
 				if (!debris.isEmpty())
-					tooltip.addPara("    Stable debris fields: %s", 3f, highlight, debris.size() + "");
+					tooltip.addPara("    Debris fields: %s", 3f, highlight, debris.size() + "");
 				if (!lowSources.isEmpty())
 					tooltip.addPara("    Weak signals: %s", 3f, highlight, lowSources.size() + "");
 				if (!medSources.isEmpty())
@@ -204,31 +201,29 @@ public class FullSpectrumSweepAbility extends BaseDurationAbility {
 		} else {
 			hasScannedCurSystem = false;
 			tooltip.addPara(
-					"Sweeps the electromagnetic spectrum to detect ambient garbage noise, radio interference, and other telltale signs of human-made artifacts across a star system. This detects the number of undiscovered objects present in the area, but not their specific nature or their exact locations.",
-					padding);
+					"Calibrates the fleet's active sensor network to search for garbage noise, abnormal emissions, and other telltale signs of human-made artifacts. This detects the number of foreign objects present in a given star system, but does not discern their specific nature or their exact locations.",
+					_padding);
 			tooltip.addPara(
-					"Discovered objects are excluded from the scan. Additionally, some phenomena - including cargo pods, unstable debris, and fleets - cannot be detected at all.",
-					padding);
+					"Objects that have already been discovered are excluded from the sweep. Additionally, some phenomena - including cargo pods, unstable debris, and any fleets other than your own - cannot be detected at all.",
+					_padding);
 			tooltip.addPara(
-					"Any given star system only needs to be scanned one time. Afterwards, an up-to-date summary for the current system can be viewed at any time through this tooltip.",
-					padding);
+					"This ability only needs to be used one time for any given system. Afterwards, this tooltip will provide an up-to-date summary while within that system.",
+					_padding);
 			tooltip.addPara(
-					"Increases the range at which the fleet can be detected by %s* units and brings the fleet to a near-stop as drives are powered down to reduce interference.",
-					10f,
+					"During activation, increases the range at which the fleet can be detected by %s* units and brings the fleet to a near-stop as drives are powered down to reduce interference.",
+					_padding,
 					Misc.getHighlightColor(),
-					new String[] {
-							Misc.getRoundedValueMaxOneAfterDecimal(DETECTABILITY_RANGE_BONUS)
-					});
+					Misc.getRoundedValueMaxOneAfterDecimal(DETECTABILITY_RANGE_BONUS));
+			tooltip.addPara("*2000 units = 1 map grid cell", gray, _padding);
+			addIncompatibleToTooltip(tooltip, expanded);
 		}
 		if (!Global.CODEX_TOOLTIP_MODE) {
 			if (fleet.isInHyperspace()) {
-				tooltip.addPara("Can not be used in hyperspace.", negative, padding);
+				tooltip.addPara("Can not be used in hyperspace.", negative, _padding);
 			} else if (fleet.getStarSystem() == null) {
-				tooltip.addPara("Must be used in a star system.", negative, padding);
+				tooltip.addPara("Must be used in a star system.", negative, _padding);
 			}
 		}
-		tooltip.addPara("*2000 units = 1 map grid cell", gray, padding);
-		addIncompatibleToTooltip(tooltip, expanded);
 	}
 
 	public static boolean ShouldCullEntry(SectorEntityToken entity) {
